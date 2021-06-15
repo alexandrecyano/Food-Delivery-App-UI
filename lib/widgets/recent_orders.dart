0d08dad61365f62f88b7482d6876067df7dcc45c
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-14 22:33:46 -0400

    Recent Order Text

---
+++ lib/widgets/recent_orders.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class RecentOrders extends StatelessWidget {
+  const RecentOrders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            'Recent Orders',
+            style: TextStyle(
+              fontSize: 24.0,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.2,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
