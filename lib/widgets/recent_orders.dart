d87c68eb3a85d6bcbd57c2812b035bad20a5a9b2
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-15 02:28:34 -0400

    worked on the recent order file

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -16,51 +16,74 @@ class RecentOrders extends StatelessWidget {
         ),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(15.0),
-            child: Image(
-              height: 100.0,
-              width: 100.0,
-              image: AssetImage(order.food.imageUrl),
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.all(12.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  order.food.name,
-                  style: TextStyle(
-                    fontSize: 18.0,
-                    fontWeight: FontWeight.bold,
+          Expanded(
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: Image(
+                    height: 100.0,
+                    width: 100.0,
+                    image: AssetImage(order.food.imageUrl),
+                    fit: BoxFit.cover,
                   ),
-                  overflow: TextOverflow.ellipsis,
                 ),
-                SizedBox(height: 4.0),
-                Text(
-                  order.restaurant.name,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          order.food.name,
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          order.restaurant.name,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(height: 4.0),
+                        Text(
+                          order.date,
+                          style: TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
                   ),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                SizedBox(height: 4.0),
-                Text(
-                  order.date,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                  overflow: TextOverflow.ellipsis,
                 ),
               ],
             ),
-          )
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            width: 48.0,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
+            child: IconButton(
+              icon: Icon(Icons.add),
+              iconSize: 30.0,
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
@@ -85,6 +108,9 @@ class RecentOrders extends StatelessWidget {
         Container(
           height: 120.0,
           child: ListView.builder(
+            //Add a boucing effect
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.only(left: 10.0),
             scrollDirection: Axis.horizontal,
             itemCount: currentUser.orders.length,
             itemBuilder: (BuildContext context, int index) {
