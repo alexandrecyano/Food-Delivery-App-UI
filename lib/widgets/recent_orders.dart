304646f08952b4403fcc0af498020eee6cd1d4d1
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-14 23:27:47 -0400

    Styling and adding image to the recent order (Recent Order Function)

--- lib/widgets/recent_orders.dart
+++ lib/widgets/recent_orders.dart
@@ -1,7 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app_ui/data/data.dart';
+import 'package:food_delivery_app_ui/models/order.dart';
 
 class RecentOrders extends StatelessWidget {
-  const RecentOrders({Key? key}) : super(key: key);
+  _buildRecentOrder(BuildContext context, Order order) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      width: 320.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
+        border: Border.all(
+          width: 1.0,
+          color: Colors.grey.shade200,
+        ),
+      ),
+      child: Row(
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image(
+              height: 100.0,
+              width: 100.0,
+              image: AssetImage(order.food.imageUrl),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +46,18 @@ class RecentOrders extends StatelessWidget {
               letterSpacing: 1.2,
             ),
           ),
-        )
+        ),
+        Container(
+          height: 120.0,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders.length,
+            itemBuilder: (BuildContext context, int index) {
+              Order order = currentUser.orders[index];
+              return _buildRecentOrder(context, order);
+            },
+          ),
+        ),
       ],
     );
   }
