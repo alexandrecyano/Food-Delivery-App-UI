30a8f2dd2847a7bf1ca0fe7efd50f91a87538758
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-16 20:22:56 -0400

    **

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app_ui/data/data.dart';
 
 class CartScreen extends StatefulWidget {
   @override
@@ -8,6 +9,10 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cart (${currentUser.cart.length})'),
+      ),
+    );
   }
 }
