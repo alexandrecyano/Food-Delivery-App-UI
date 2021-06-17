2560879907ed703cc75cf280726a03977b4156c4
Alexandre Yano <alexandreyano@hotmail.com>
2021-06-16 22:32:26 -0400

    **

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -7,105 +7,115 @@ class CartScreen extends StatefulWidget {
   _CartScreenState createState() => _CartScreenState();
 }
 
-_buildCartItem(Order order) {
-  return Container(
-    padding: EdgeInsets.all(20.0),
-    height: 170.0,
-    child: Row(
-      children: <Widget>[
-        Expanded(
-          child: Row(
-            children: <Widget>[
-              Container(
-                width: 150,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage(order.food.imageUrl),
-                      fit: BoxFit.cover),
-                  borderRadius: BorderRadius.circular(15.0),
+class _CartScreenState extends State<CartScreen> {
+  _buildCartItem(Order order) {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      height: 170.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  width: 150,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage(order.food.imageUrl),
+                        fit: BoxFit.cover),
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.all(12.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(order.food.name,
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(order.food.name,
+                            style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            overflow: TextOverflow.ellipsis),
+                        SizedBox(height: 10.0),
+                        Text(
+                          order.restaurant.name,
                           style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w600,
                           ),
-                          overflow: TextOverflow.ellipsis),
-                      SizedBox(height: 10.0),
-                      Text(
-                        order.restaurant.name,
-                        style: TextStyle(
-                          fontSize: 16.0,
-                          fontWeight: FontWeight.w600,
+                          overflow: TextOverflow.ellipsis,
                         ),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      SizedBox(height: 10.0),
-                      Container(
-                        width: 100.0,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10.0),
-                          border: Border.all(
-                            width: 0.8,
-                            color: Colors.black54,
+                        SizedBox(height: 10.0),
+                        Container(
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10.0),
+                            border: Border.all(
+                              width: 0.8,
+                              color: Colors.black54,
+                            ),
                           ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            GestureDetector(
-                              onTap: () {},
-                              child: Text(
-                                '_',
-                                style: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 20.0,
-                                    fontWeight: FontWeight.w600),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '_',
+                                  style: TextStyle(
+                                      color: Theme.of(context).primaryColor,
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w600),
+                                ),
                               ),
-                            ),
-                            SizedBox(width: 20.0),
-                            Text(
-                              order.quantity.toString(),
-                              style: TextStyle(
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            SizedBox(width: 20.0),
-                            GestureDetector(
-                              onTap: () {},
-                              child: Text(
-                                '_',
+                              SizedBox(width: 20.0),
+                              Text(
+                                order.quantity.toString(),
                                 style: TextStyle(
-                                  color: Theme.of(context).primaryColor,
                                   fontSize: 20.0,
                                   fontWeight: FontWeight.w600,
                                 ),
                               ),
-                            ),
-                          ],
+                              SizedBox(width: 20.0),
+                              GestureDetector(
+                                onTap: () {},
+                                child: Text(
+                                  '_',
+                                  style: TextStyle(
+                                    color: Theme.of(context).primaryColor,
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
-        )
-      ],
-    ),
-  );
-}
+          Container(
+            margin: EdgeInsets.all(10.0),
+            child: Text(
+              '\$${order.quantity * order.food.price}',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
